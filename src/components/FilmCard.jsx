34f7a3cf373a52d2e9a789a8323ed4bf7b35ffe7
tatymediina <tatimedinaiuni@gmail.com>
2025-05-12 08:38:38 -0300

    feat: :zap: Se realizaron cambios para que el codigo quede más organizado y limpio

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,11 +1,7 @@
 function FilmCard({img, titulo,año}){
     return(
         <div className="FilmCard">
-            
-
             <img src={img} alt={titulo} className="img-film"/>
-            
-           
             <h3 className="titulo-film">{titulo}</h3>
             <p className="año-film">{año}</p>
         </div>
---
+++ src/pages/data.js
@@ -0,0 +1,83 @@
+
+export const peliculas = [
+  {
+    img: "https://i.pinimg.com/736x/7f/e2/cd/7fe2cd5f0ba6f5d4f50becb9e461a0d9.jpg",
+    titulo: "Mi vecino Totoro",
+    año: "1988",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/tumba-luciernagas-3310393.jpg",
+    titulo: "La tumba de las luciérnagas",
+    año: "1988",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/castillo-ambulante-3310382.jpg",
+    titulo: "El Castillo ambulante",
+    año: "2004",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/viaje-chihiro-3310384.jpg",
+    titulo: "El viaje de Chihiro",
+    año: "2001",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/recuerdo-marnie-3310375.jpg",
+    titulo: "El recuerdo de Marnie",
+    año: "2014",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/chico-garza-3310373.jpg",
+    titulo: "El chico y la garza",
+    año: "2023",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/princesa-mononoke-3310386.jpg?tf=828x",
+    titulo: "Princesa Mononoke",
+    año: "1997",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/nausicaa-valle-viento-3310396.jpg?tf=828x",
+    titulo: "Nausicaä del Valle del Viento",
+    año: "1984",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/ponyo-acantilado-3310380.jpg?tf=828x",
+    titulo: "Ponyo en el acantilado",
+    año: "2008",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/cuento-princesa-kaguya-3310377.jpg?tf=828x",
+    titulo: "El cuento de la princesa Kaguya",
+    año: "2013",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/susurros-corazon-3310387.jpg?tf=828x",
+    titulo: "Susurros del corazón",
+    año: "1995",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/arrietty-mundo-diminutos-3310379.jpg?tf=828x",
+    titulo: "Arrietty y el mundo de los diminutos",
+    año: "2010",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/colina-amapolas-3310378.jpg?tf=828x",
+    titulo: "Colina de Amapolas",
+    año: "2011",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/haru-reino-gatos-3310383.jpg?tf=828x",
+    titulo: "Haru en el reino de los gatos",
+    año: "2002",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/nicky-aprendiz-bruja-3310392.jpg?tf=828x",
+    titulo: "Kiki: Entregas a domicilio",
+    año: "1989",
+  },
+  {
+    img: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2024/05/porco-rosso-3310390.jpg?tf=828x",
+    titulo: "Porco Rosso",
+    año: "1992",
+  },
+];
